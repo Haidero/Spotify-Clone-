4fb039cb91cab697243a329c797aad114a82dabf
Zeeshan Haider Ali <haiderjaan713@gmail.com>
2025-07-25 16:26:33 +0500

    html and css done

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("wellcome to spotify");
+let songs = [
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+    {sonName: "salam", filePath: "songs/2.mp3", coverPath: ""},
+]
+let  audioElement = new Audio('songs/1.mp3');
+// audioElement.play();
